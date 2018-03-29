588fc96775de1c776c67288dd708473d8b41cc3b
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-03-29 09:49:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# promise-splittetr
+
+<div>
+	<a href="https://www.npmjs.com/package/promise-splittetr"><img src='http://img.shields.io/npm/v/promise-splittetr.svg?style=flat'></a>
+	<a href="https://www.npmjs.com/package/promise-splittetr"><img src='https://img.shields.io/npm/dm/promise-splittetr.svg?style=flat-square'></a>
+	<a href="https://david-dm.org/giowe/promise-splittetr"><img src='https://david-dm.org/giowe/promise-splittetr/status.svg'></a>
+	<a href="https://www.youtube.com/watch?v=Sagg08DrO5U"><img src='http://img.shields.io/badge/gandalf-approved-61C6FF.svg'></a>
+</div>
+
+Split a promise list into chunks and exectes them synchronously with a retry system
+
+## Options
+```
+{
+  chunkSize: 5,
+  retryLimit: 3,
+  startChunk: 1,
+  verbose: false
+}
+```
---
+++ index.js
@@ -0,0 +1,49 @@
+const splitter = (promises, options, cb) => new Promise(async (resolve, reject) => {
+  if (!promises.length) return resolve();
+
+  const defaultOptions = {
+    chunkSize: 5,
+    retryLimit: 3,
+    startChunk: 1,
+    verbose: false
+  };
+
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  } else if (typeof options === 'undefined') {
+    options = {};
+  }
+
+  options = Object.assign(defaultOptions, options);
+
+  const { chunkSize, retryLimit, verbose, startChunk } = options;
+
+  const l = promises.length;
+  const chunkTotal = Math.ceil(l / chunkSize);
+  const out = [];
+  if (verbose) console.log(`Splitting promise list in ${chunkTotal} chunk${chunkTotal > 1 ? 's': ''} of ${chunkSize} promise${chunkSize > 1 ? 's' : ''}, starting from chunk ${startChunk}.`);
+  for (let i = (startChunk - 1) * chunkSize; i < l; i+=chunkSize) {
+    const chunkIndex = i / chunkSize + 1;
+    let retryCount = retryLimit || 1;
+    if (verbose) console.log(`chunk ${chunkIndex}`);
+    while (retryCount) try {
+      const result = await Promise.all(promises.slice(i, i + chunkSize).map(fn => fn(chunkIndex)));
+      out.push(result);
+      if (cb) cb(null, result);
+      break;
+    } catch (err) {
+      err.chunkIndex = chunkIndex;
+      console.log(err);
+      console.log(`retry ${retryLimit - retryCount + 1}/${retryLimit}...`);
+      retryCount--;
+      if (retryCount === 0) {
+        return reject(err);
+      }
+      if (cb) cb(err);
+    }
+  }
+  resolve(out);
+});
+
+module.exports = splitter;
