eb06a6e4fbf1024d9c55dcbaea1c7c7bb12d882f
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-03-30 11:38:51 +0200

    added logger

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const splitter = (promises, options, cb) => new Promise(async (resolve, reject) 
     chunkSize: 5,
     retryLimit: 3,
     startChunk: 1,
-    verbose: false
+    verbose: false,
+    logger: {
+      log: console.log,
+      error: console.error
+    }
   };
 
   if (typeof options === 'function') {
@@ -22,11 +26,11 @@ const splitter = (promises, options, cb) => new Promise(async (resolve, reject) 
   const l = promises.length;
   const chunkTotal = Math.ceil(l / chunkSize);
   const out = [];
-  if (verbose) console.log(`Splitting promise list in ${chunkTotal} chunk${chunkTotal > 1 ? 's': ''} of ${chunkSize} promise${chunkSize > 1 ? 's' : ''}, starting from chunk ${startChunk}.`);
+  if (verbose) logger.log(`Splitting promise list in ${chunkTotal} chunk${chunkTotal > 1 ? 's': ''} of ${chunkSize} promise${chunkSize > 1 ? 's' : ''}, starting from chunk ${startChunk}.`);
   for (let i = (startChunk - 1) * chunkSize; i < l; i+=chunkSize) {
     const chunkIndex = i / chunkSize + 1;
     let retryCount = retryLimit || 1;
-    if (verbose) console.log(`chunk ${chunkIndex}`);
+    if (verbose) logger.log(`chunk ${chunkIndex}`);
     while (retryCount) try {
       const result = await Promise.all(promises.slice(i, i + chunkSize).map(fn => fn(chunkIndex)));
       out.push(result);
@@ -34,8 +38,8 @@ const splitter = (promises, options, cb) => new Promise(async (resolve, reject) 
       break;
     } catch (err) {
       err.chunkIndex = chunkIndex;
-      console.log(err);
-      console.log(`retry ${retryLimit - retryCount + 1}/${retryLimit}...`);
+      logger.error(err);
+      logger.log(`retry ${retryLimit - retryCount + 1}/${retryLimit}...`);
       retryCount--;
       if (retryCount === 0) {
         return reject(err);
